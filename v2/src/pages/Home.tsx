e701ef9a1ef73d76341cc0ef3decd75752c6a4f3
Manuareraa <manuareraa@gmail.com>
2024-09-11 16:51:16 +0530

    Update Home.tsx

--- v2/src/pages/Home.tsx
+++ v2/src/pages/Home.tsx
@@ -4,41 +4,44 @@ import HeroCards from "../components/custom/HeroCards";
 function Home() {
   return (
     <div className="flex flex-col">
-      {/* top hero container */}
-      <div className="flex flex-row items-center justify-between px-4 space-x-4 space-y-4">
-        {/* left container */}
-        <div className="flex flex-col items-start pl-8 text-left gap-y-6">
-          {/* text container */}
-          <div className="">
-            <div className="mb-3 text-4xl">The New Age of Investment</div>
-            <div className="flex flex-col gap-y-3 text-8xl">
-              <div className="font-bold ">Own Real Estate Shares</div>
-              <div className="font-bold ">Own the Future</div>
+      {/* Top hero container */}
+      <div className="container flex flex-col items-center justify-between px-4 mx-auto space-y-8 md:flex-row md:space-x-4 md:space-y-0">
+        {/* Left container */}
+        <div className="flex flex-col items-start pl-4 text-left md:pl-8 gap-y-6">
+          {/* Text container */}
+          <div>
+            <div className="mb-3 text-3xl md:text-4xl">
+              The New Age of Investment
+            </div>
+            <div className="flex flex-col text-5xl gap-y-3 md:text-8xl">
+              <div className="font-bold">Own Real Estate Shares</div>
+              <div className="font-bold">Own the Future</div>
             </div>
           </div>
 
-          {/* button container */}
-          <div className="flex flex-row my-3 gap-x-8 gap-">
-            <button className="px-6 py-2 text-black bg-white border-[3px] w-48 border-black rounded-full hover:bg-black hover:text-white">
+          {/* Button container */}
+          <div className="flex flex-row my-3 gap-x-4 md:gap-x-8">
+            <button className="px-4 py-2 md:px-6 md:py-3 text-black bg-white border-[3px] w-40 md:w-48 border-black rounded-full hover:bg-black hover:text-white">
               Get Started
             </button>
-            <button className="px-6 py-2 text-white bg-black border-[3px] w-48 border-black rounded-full hover:bg-white hover:text-black">
+            <button className="px-4 py-2 md:px-6 md:py-3 text-white bg-black border-[3px] w-40 md:w-48 border-black rounded-full hover:bg-white hover:text-black">
               Get Started
             </button>
           </div>
         </div>
 
-        {/* right container */}
-        <div>
+        {/* Right container */}
+        <div className="w-full max-w-md md:max-w-lg lg:max-w-xl">
           <img
             src={heroImage}
             alt="placeholder"
-            className="h-auto w-[40rem] cover"
+            className="object-cover w-full h-auto"
           />
         </div>
       </div>
-      {/* bottom container */}
-      <div className="flex flex-col items-center justify-center w-full px-8">
+
+      {/* Bottom container */}
+      <div className="flex flex-col items-center justify-center w-full px-4 mt-8 md:px-8">
         <HeroCards />
       </div>
     </div>
